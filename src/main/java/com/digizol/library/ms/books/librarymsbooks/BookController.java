f5ecfb58126158aaec08fb0bb7f5ea07f11e4af8
Kamal Mettananda <kamal_mettananda@persistent.com>
2022-09-07 17:24:22 +0530

    Included actuator to support liveliness and readiness probes. Also added few log lines into the controller.

--- src/main/java/com/digizol/library/ms/books/librarymsbooks/BookController.java
+++ src/main/java/com/digizol/library/ms/books/librarymsbooks/BookController.java
@@ -1,5 +1,7 @@
 package com.digizol.library.ms.books.librarymsbooks;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +12,13 @@ import java.util.List;
 @RequestMapping("/books")
 public class BookController {
 
+    private Logger logger = LoggerFactory.getLogger(BookController.class);
+
     @GetMapping ("")
     public List<Book> list() {
+        logger.debug("Start method list()");
+        logger.info("Returning books, size = 4");
+        logger.debug("End method list()");
         return List.of(
                 new Book(1, "Software Architecture in Practice"),
                 new Book(2, "Designing Software Architectures"),
